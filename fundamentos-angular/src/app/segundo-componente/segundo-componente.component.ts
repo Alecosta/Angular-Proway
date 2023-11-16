d473a145977268ff346fda798dc6d1f5a65b5ed2
AleCosta <alecostabr@gmail.com>
2023-11-16 16:22:05 -0300

    criando componente manualmente

---
+++ fundamentos-angular/src/app/segundo-componente/segundo-componente.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-segundo-componente",
+  template: "<h2>Segundo Componente</h2>",
+  styles: ["h2 { color: blue;} "]
+})
+
+
+export class SegundoComponenteComponent {}
+
